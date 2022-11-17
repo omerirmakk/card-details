baf7a19cc5d3d3ff96ce69905337a7fbfb9ed58b
Omer Faruk IRMAK <omerfarukirmak@stu.ayvansaray.edu.tr>
2022-11-18 02:55:06 +0300

    Can usable in mobile and desktop Finished with all details as given in the guide Done with pure Js cause of that hasn't done the last part (Confirm)

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const ccvNumber = document.querySelector('.cvcnumber');
 const submit = document.querySelector('.submit');
 const cardNumber = document.querySelector('.card-number');
 const cardNumber2 = document.querySelector('.card-number2');
-
+const expDateMonth = document.querySelector('.exp-date');
+const expDateYear = document.querySelector('.exp-date2');
+const expDateHolderMonth = document.querySelector('.month');
+const expDateHolderYear = document.querySelector('.year');
 // ccvNumber.addEventListener('input', () => {
 //   ccv.textContent = ccvNumber.value;
 // });
@@ -23,7 +26,9 @@ cardNumber2.oninput = function () {
 cardName2.oninput = function () {
   changeValue(cardName, cardName2);
 };
-
-submit.addEventListener('click', () => {
-  ccv.textContent = ccvNumber.value;
-});
+expDateMonth.oninput = function () {
+  changeValue(expDateHolderMonth, expDateMonth);
+};
+expDateYear.oninput = function () {
+  changeValue(expDateHolderYear, expDateYear);
+};
